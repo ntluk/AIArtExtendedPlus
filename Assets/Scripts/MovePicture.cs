604f46ae5d3b1ea1570206170ccc4b1d0ef3d70c
SorenAli <soren_ali_123@hotmail.com>
2023-11-29 13:58:38 +0100

    cleancode

--- Assets/Scripts/MovePicture.cs
+++ Assets/Scripts/MovePicture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d10e30541015603ec22d2bccf6f7e092887c370c2a49a777e2d57de26f34ccdd
-size 49174
+oid sha256:8fe6fe2330458d1b6e2bb14034f6744bc9414e44bf0788491921bbded31e2fe6
+size 36867
