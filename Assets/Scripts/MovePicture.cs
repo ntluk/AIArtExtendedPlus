07945579a755399e156fd47ba26fe9ad37f55acb
SorenAli <soren_ali_123@hotmail.com>
2024-01-07 17:05:09 +0100

    Liken mit Rechter und Linker Hand möglich // prio Rechtehand

--- Assets/Scripts/MovePicture.cs
+++ Assets/Scripts/MovePicture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e60e0c9331078de50fe7414fd64bd3ce77ebbbba82e30dc4319720814c96724
-size 54119
+oid sha256:e8b9dcefccdd936c5ea9442b4e4b4da89ed8b788382d27a14ccf7a44a43673af
+size 55350
---
+++ Assets/Scripts/VelocityLeft.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b3b26886014e603b4eda26e70717e266e1ae6b9bc46974ad5c7b8330c42fb7ad
+size 2378
