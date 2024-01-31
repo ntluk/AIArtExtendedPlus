064d4c37e0f76fa13ddb496c27b5dbd9513b9a01
ntluk <niklas.tluk@study.hs-duesseldorf.de>
2024-01-31 15:47:06 +0100

    bug fixes
    bug fixes

--- Assets/Scripts/GetData.cs
+++ Assets/Scripts/GetData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae268917a6b5ef85c86d5d5b7ab103d9fb31051e54053dc345ccc47b7e2f513f
-size 2964
+oid sha256:f3372b9a8e460ab9bc7b4f93c702eaaf44f8dde28db0531365671defdd1f313d
+size 3756
