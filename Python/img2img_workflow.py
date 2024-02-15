aa9f4a7993e7414c9ada62d1fde1fd94d157b7bb
ntluk <niklas.tluk@study.hs-duesseldorf.de>
2024-02-15 17:24:53 +0100

    updated pipeline + new images

--- Python/img2img_workflow.py
+++ Python/img2img_workflow.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ac9422efdd20b798a084708f956d12144ae0cbb9618ea4dc4aacb33aec3d781
-size 448
+oid sha256:d9c15f2904e31f62f69d4e09df52556635e63fdebf97502544f8ea7d3e713233
+size 457
--- Python/txt2img_workflow.py
+++ Python/txt2img_workflow.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bf8dfa58e7930c400767e08bb30870ab96afedd8c9780de91dc9e6b099aa291
-size 447
+oid sha256:5a258e676ea44d33e2dea7ca8225197c049e0b250b4ff13a62d8752c769210e3
+size 459
