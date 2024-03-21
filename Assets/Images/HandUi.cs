18dc419da85e8f81f0bbd9c3cad591bda7c0127e
ntluk <niklas.tluk@study.hs-duesseldorf.de>
2024-03-21 15:31:39 +0100

    HandUi

---
+++ Assets/Images/HandUi.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:99b0a516d9a61a7a16e75f8cf82853d1b55cc70eccf47c5f668df2713589c17a
+size 2971
--- Assets/Scripts/MovePicture.cs
+++ Assets/Scripts/MovePicture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:436fddda3ae73796c11385ce089b6ad3f77ae7ad5f8e28a7f3aafa58890bd5d6
-size 65190
+oid sha256:a969f1482405328cef27a90fdd6a717dfe7de3508eceac046964fc74a8f7f38f
+size 65293
