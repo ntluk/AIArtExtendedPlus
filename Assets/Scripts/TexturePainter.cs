82366bc2fab5f7a22f76aa5397a557f69c6805b2
Niklas Tluk <niklas.tluk@study.hs-duesseldorf.de>
2024-07-15 18:13:38 +0200

    input system setup for touch controls

--- Assets/Scripts/TexturePainter.cs
+++ Assets/Scripts/TexturePainter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a060b3416f6f4eb2250aa9bdcb5cd42afc9cd8d922aa69e2dc6f6094e42b38d5
-size 15685
+oid sha256:ce867ac17788e4badcf0bb3b436cd440538eede0901b45661f3b074a45e96911
+size 16228
