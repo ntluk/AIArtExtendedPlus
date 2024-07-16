8919713c696e93a5751de904af9f18014e36442a
ntluk <niklas.tluk@study.hs-duesseldorf.de>
2024-07-16 12:44:21 +0200

    WIP touch controls

--- Assets/Scripts/DrawCube.cs
+++ Assets/Scripts/DrawCube.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11b1a7d59cac201dd21bc15fdae302100abcbb7ea59fb7f1d96299b08621cbcd
-size 5619
+oid sha256:16633f62a407b5f0c43c013519aa11375d601ad2f4f5c60562305efed6b521f3
+size 7504
--- Assets/Scripts/TexturePainter.cs
+++ Assets/Scripts/TexturePainter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce867ac17788e4badcf0bb3b436cd440538eede0901b45661f3b074a45e96911
-size 16228
+oid sha256:71627f5aa76e7b6b277abc01052041773fb249cdd4b03f78b7e22fa005746c3b
+size 16471
