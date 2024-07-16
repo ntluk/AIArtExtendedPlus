f5752de4a6066dfa6dfde0a3288f3b72c69851ae
ntluk <niklas.tluk@study.hs-duesseldorf.de>
2024-07-16 16:02:26 +0200

    draws on click pos only

--- Assets/Scripts/TexturePainter.cs
+++ Assets/Scripts/TexturePainter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce8bf8218033ac96fa5bac6a6d36a562e4eabda8414d3324740c8c4f65476dfe
-size 17228
+oid sha256:e4b52e9a56f04eb02ab4abb58d9f2e78d7dc4b1545f17eaf195d661ebfecf134
+size 17150
