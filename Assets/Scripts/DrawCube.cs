40520de3edbc0cda0840ee4defaf81af6780f274
ntluk <niklas.tluk@study.hs-duesseldorf.de>
2024-07-16 13:37:35 +0200

    control draw cube via touch input

--- Assets/Scripts/DrawCube.cs
+++ Assets/Scripts/DrawCube.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16633f62a407b5f0c43c013519aa11375d601ad2f4f5c60562305efed6b521f3
-size 7504
+oid sha256:9324fa189d7e127c1498a61d1a1ee52ce0e1d872f4f228b50fb90787308c6f32
+size 6430
--- Assets/Scripts/TexturePainter.cs
+++ Assets/Scripts/TexturePainter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:71627f5aa76e7b6b277abc01052041773fb249cdd4b03f78b7e22fa005746c3b
-size 16471
+oid sha256:d443d985cf6afd5d53afb0e528794abe1f499b0afeafa2ec7598d23b0285ec6b
+size 16716
