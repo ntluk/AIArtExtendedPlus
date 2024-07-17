d85fc4c4e94d581453100f0c59885b50fb2fa10d
ntluk <niklas.tluk@study.hs-duesseldorf.de>
2024-07-17 16:19:39 +0200

    working temp solution

--- Assets/Scripts/TexturePainter.cs
+++ Assets/Scripts/TexturePainter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4b52e9a56f04eb02ab4abb58d9f2e78d7dc4b1545f17eaf195d661ebfecf134
-size 17150
+oid sha256:a8d998d5553ab87aa31ea1137e57916693662318fca192ca614ce1e5720552d1
+size 15070
