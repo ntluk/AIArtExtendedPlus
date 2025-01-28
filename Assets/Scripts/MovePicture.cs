31ee3afef03853c8374c0b7ea6e1c57fb6190358
Soren <soren_ali_123@hotmail.com>
2025-01-28 16:27:02 +0100

    Updated: Fertuge Anwendung

--- Assets/Scripts/MovePicture.cs
+++ Assets/Scripts/MovePicture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65b0f37e8d0a49dd823e92beca2af11ab9abe5b2060d3c747f4648bc9bc8f58b
-size 47632
+oid sha256:559307d1d53d462425fe794ba32a6fc075cbb4c40abbc6b4012d79d5b12b5752
+size 48191
--- Assets/Scripts/VideoPlay.cs
+++ Assets/Scripts/VideoPlay.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f78e04774d960fcffac0625d6d95612240a6fafcea8b46704a6b7a4cce17a09f
-size 12290
+oid sha256:817640058be2bcf93264be733c7e34570d1341695116c765bf4c17dc1d4285fb
+size 12454
