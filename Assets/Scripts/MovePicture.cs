8ffbec3dd7da8a96e8ad62af3a99852d01fd83a2
Soren <soren_ali_123@hotmail.com>
2025-03-17 09:25:22 +0100

    UPD

--- Assets/Scripts/MovePicture.cs
+++ Assets/Scripts/MovePicture.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ed7d264cee5481cb0acbd4a69e6d9db240784012b6f9ec870d40a5cc2e7e8ac
-size 26424
+oid sha256:7eadb4f6ab47b6d53efa32ba75f4d26750ba998eaa0edf4a94cef5578d7f0603
+size 26473
--- Assets/Scripts/VideoPlay.cs
+++ Assets/Scripts/VideoPlay.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a1bc0298b68fe182a57a1bdee89a51b2697f411176800c032df8eda72603dff
-size 8748
+oid sha256:24de73ed563ebe8552807364fc5e3d7f0ad9960f600e1d2bb8b046ce192295ec
+size 8825
